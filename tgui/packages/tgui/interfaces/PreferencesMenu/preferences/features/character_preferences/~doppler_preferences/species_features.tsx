6846072c2c7dc6e49fc2d8f510d94d41e4731527
Lila S. <sheepwiththemask@gmail.com>
2024-09-03 17:36:13 +0200

    ok idk

--- tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/~doppler_preferences/species_features.tsx
+++ tgui/packages/tgui/interfaces/PreferencesMenu/preferences/features/character_preferences/~doppler_preferences/species_features.tsx
@@ -1,15 +1,7 @@
-import {
-  FeatureChoiced,
-  FeatureChoicedServerData,
-  FeatureValueProps,
-} from '../../base';
+import { FeatureChoiced } from '../../base';
 import { FeatureDropdownInput } from '../../dropdowns';
 
 export const feature_animalistic: FeatureChoiced = {
   name: 'Animalistic trait',
-  component: (
-    props: FeatureValueProps<string, string, FeatureChoicedServerData>,
-  ) => {
-    return <FeatureDropdownInput buttons {...props} />;
-  },
+  component: FeatureDropdownInput,
 };
